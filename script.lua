259518c2adcd07591eb2e33658b7c78f39fb770b
growagardenhub-debug <sinathmeas455@gmail.com>
2025-06-24 10:09:27 -0500

    Update script.lua

--- script.lua
+++ script.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/growagardenhub-debug/weas/refs/heads/main/fww"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/growagardenhub-debug/faefae/refs/heads/main/fae"))()
